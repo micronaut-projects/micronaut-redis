22944c5e510d90519a81966a46864274b37acbd0
Tim Yates <tim.yates@gmail.com>
2022-03-14 11:51:15 +0000

    Remove unused imports from Test

--- redis-lettuce/src/test/groovy/io/micronaut/configuration/lettuce/cache/RedisPoolCacheSpec.groovy
+++ redis-lettuce/src/test/groovy/io/micronaut/configuration/lettuce/cache/RedisPoolCacheSpec.groovy
@@ -1,25 +1,5 @@
-/*
- * Copyright 2017-2019 original authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package io.micronaut.configuration.lettuce.cache
 
-import io.lettuce.core.api.StatefulRedisConnection
-import io.lettuce.core.api.async.RedisStringAsyncCommands
-import io.lettuce.core.codec.StringCodec
-import io.lettuce.core.protocol.AsyncCommand
-import io.lettuce.core.protocol.RedisCommand
 import io.lettuce.core.support.AsyncPool
 import io.micronaut.context.ApplicationContext
 import io.micronaut.context.BeanLocator
@@ -28,15 +8,9 @@ import io.micronaut.core.convert.DefaultConversionService
 import io.micronaut.core.type.Argument
 import io.micronaut.inject.qualifiers.Qualifiers
 import io.micronaut.runtime.ApplicationConfiguration
-import io.netty.buffer.ByteBuf
-import io.netty.buffer.PooledUnsafeDirectByteBuf
-import spock.lang.Requires
 import spock.lang.Specification
 
-import java.lang.reflect.Field
-import java.lang.reflect.Modifier
 import java.nio.charset.Charset
-import java.util.concurrent.ExecutionException
 
 /**
  * @author Kovalov Illia

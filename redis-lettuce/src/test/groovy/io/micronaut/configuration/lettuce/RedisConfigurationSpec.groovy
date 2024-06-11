ca9b8bab8923cd1de5f9bf049ef43c3b3d01c3ef
Suneet Kamath <suneet.kamath@msncorp.net>
2024-06-11 14:14:14 +0100

    check that drivers are not available when disabled

--- redis-lettuce/src/test/groovy/io/micronaut/configuration/lettuce/RedisConfigurationSpec.groovy
+++ redis-lettuce/src/test/groovy/io/micronaut/configuration/lettuce/RedisConfigurationSpec.groovy
@@ -1,5 +1,7 @@
 package io.micronaut.configuration.lettuce
 
+import io.lettuce.core.api.StatefulRedisConnection
+import io.lettuce.core.RedisClient
 import io.micronaut.context.ApplicationContext
 import io.micronaut.context.exceptions.NoSuchBeanException
 
@@ -10,12 +12,34 @@ import spock.lang.Specification
 class RedisConfigurationSpec extends Specification {
     @AutoCleanup ApplicationContext applicationContext
 
-    void "test disabled"() {
+    void "test AbstractRedisConfiguration not available when disabled"() {
         given:
         applicationContext = ApplicationContext.run(["redis.enabled": false])
 
         when:
         applicationContext.getBean(AbstractRedisConfiguration)
+
+        then:
+        thrown(NoSuchBeanException)
+    }
+
+    def "test StatefulRedisConnection not available when disabled"() {
+        given:
+        applicationContext = ApplicationContext.run(['redis.enabled': false])
+
+        when:
+        applicationContext.getBean(StatefulRedisConnection)
+
+        then:
+        thrown(NoSuchBeanException)
+    }
+
+    def "test RedisClient not available when disabled"() {
+        given:
+        applicationContext = ApplicationContext.run(['redis.enabled': false])
+
+        when:
+        applicationContext.getBean(RedisClient)
 
         then:
         thrown(NoSuchBeanException)
